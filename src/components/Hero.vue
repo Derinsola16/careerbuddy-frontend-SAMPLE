ec7d70695522b9fe0e779accbeb4228f0783c135
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2023-02-17 11:22:54 +0100

    needed to commit but not done

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,58 +4,43 @@
     <div class="md:flex items-center justify-between w-full">
       <div class="md:w-1/2 lg:pr-28">
         <Label class="flex" v-if="labelText" :labelText="labelText" />
-        <p :class="[white ? 'text-white' : 'text-brand-black']" class="text-5xl md:-text-4xl md:w-auto lg:text-[60px] font-albertExtraBold lg:leading-[64px] mb-5 md:mb-3">{{ title }}</p>
+        <p
+          :class="[white ? 'text-white' : 'text-brand-black']"
+          class="text-5xl md:-text-4xl md:w-auto lg:text-[60px] font-albertExtraBold lg:leading-[64px] mb-5 md:mb-3"
+        >
+          {{ title }}
+        </p>
         <p class="font-albertSmall mb-8 pr-12 md:pr-0" :class="[white ? 'text-white' : 'text-brand-black']">
           {{ caption }}
         </p>
         <slot name="button" />
       </div>
-      <div class="self-start md:w-1/2">
-        <VueCardStack
-          :cards="cards"
-          :card-width="350"
-          :card-height="350"
-          :stack-width="stackWidth"
-          :max-visible-cards="parseInt(maxVisibleCards)"
-          :scale-multiplier="parseFloat(scaleMultiplier)"
-          ref="stack"
-        >
-          <template v-slot:card="{ card }">
-            <img style="width: 100%; height: 100%" :src="card.background" alt="hero" class="rounded-xl object-cover" />
-          </template>
-        </VueCardStack>
+      <!-- image slider  -->
+      <div class="self-start md:w-1/2 md:flex justify-center">
+        <div class="relative flex items-center">
+          <!-- light divs  -->
+          <div class="w-[480px] h-[420px] absolute top-0 left-0 bg-[#D9D9D9] opacity-40 ml-9 rounded-lg mt-[15px]"></div>
+          <div class="w-[460px] h-[390px] absolute top-0 left-0 bg-[#D9D9D9] opacity-20 ml-[72px] rounded-lg mt-[30px]"></div>
+          <div class="w-[440px] h-[360px] absolute top-0 left-0 bg-[#D9D9D9] opacity-10 ml-[110px] rounded-lg mt-[45px]"></div>
+          <!-- image and overflow info  -->
+          <div class="relative w-[500px] h-[450px] overflow-hidden rounded-lg">
+            <img src="../assets/images/hero.png" class="w-full h-full object-cover" alt="image" />
+          </div>
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script>
-import VueCardStack from '@/components/cardStack/CardStack.vue';
 import Label from '@/components/Label.vue';
-import { debounce } from '@/utils/debounce';
 export default {
-  props: { cards: Array, title: String, caption: String, labelText: String, white: Boolean },
+  props: { title: String, caption: String, labelText: String, white: Boolean },
   components: {
-    VueCardStack,
     Label,
   },
   data() {
-    return {
-      maxVisibleCards: 5,
-      containerWidth: 470,
-      scaleMultiplier: 1,
-    };
-  },
-  computed: {
-    stackWidth: {
-      get() {
-        return this.containerWidth;
-      },
-      set: debounce(function (val) {
-        this.containerWidth = parseInt(val);
-        this.$refs.stack.rebuild();
-      }, 100),
-    },
+    return {};
   },
 };
 </script>
